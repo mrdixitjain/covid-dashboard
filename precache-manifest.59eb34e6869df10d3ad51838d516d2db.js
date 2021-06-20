f1752cf23e482dd33466404bc9dcfa67119ca09d
mrdixitjain <mrdixitjain1999@gmail.com>
2021-06-20 13:36:20 +0530

    Updates

---
+++ precache-manifest.59eb34e6869df10d3ad51838d516d2db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dbb95a4bdafb6a628e0f9fd71d6542e",
+    "url": "/covid-dashboard/index.html"
+  },
+  {
+    "revision": "0f101fdac027bec4c354",
+    "url": "/covid-dashboard/static/css/2.f7023ff7.chunk.css"
+  },
+  {
+    "revision": "b0ff0ab84cf255e9b99b",
+    "url": "/covid-dashboard/static/css/main.f471f090.chunk.css"
+  },
+  {
+    "revision": "0f101fdac027bec4c354",
+    "url": "/covid-dashboard/static/js/2.dc803911.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/covid-dashboard/static/js/2.dc803911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ff0ab84cf255e9b99b",
+    "url": "/covid-dashboard/static/js/main.7e11f2ef.chunk.js"
+  },
+  {
+    "revision": "de6a3dd7f8409eb9afff",
+    "url": "/covid-dashboard/static/js/runtime-main.8f1ec971.js"
+  }
+]);
